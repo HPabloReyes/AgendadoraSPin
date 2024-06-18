774ddbcfc9b2d8ded255f145dfb9812bbae7132c
Pablo Reyes <chugo940@gmail.com>
2024-06-17 22:40:05 -0600

    trabajando en feching

--- app/Citas/page.js
+++ app/Citas/page.js
@@ -1,15 +1,16 @@
 export default async function Citas() {
-  const res = await fetch(
-    "http://localhost:3000/api/citas" ||
-      "https://agendadora-s-pin.vercel.app/api/citas"
-  );
-  const json = await res.json();
-  console.log(json);
-  return (
-    <h1>
-      {json.map((e) => (
-        <div key={e._id}>{e.tienda}</div>
-      ))}
-    </h1>
-  );
+  //   const res = await fetch(
+  //     "http://localhost:3000/api/citas" ||
+  //       "https://agendadora-s-pin.vercel.app/api/citas"
+  //   );
+  //   const json = await res.json();
+  //   console.log(json);
+  //   return (
+  //     <h1>
+  //       {json.map((e) => (
+  //         <div key={e._id}>{e.tienda}</div>
+  //       ))}
+  //     </h1>
+  //   );
+  <h1>Seguimos trabajando en esto</h1>;
 }
